a1969009ae0a6a39cb9a9f4dbadb33ee63f9e86f
Paul Cunningham <pjcunningham@borsuk.co.uk>
2021-12-08 18:13:07 +0000

    Add command help

--- sageodbc/cli.py
+++ sageodbc/cli.py
@@ -41,11 +41,17 @@ def about():
 
 
 @cli.command('dump-table')
-@click.argument('table_name', type=str)
+@click.argument('table-name', type=str)
 @click.argument('save-to', type=click.Path(exists=False, dir_okay=False, file_okay=True))
 @click.argument('output-format', type=OutputFormatType(OutputFormatEnum))
 @pass_manager
 def dump_table(manager: Manager, table_name: str, save_to: str, output_format: OutputFormatEnum):
+    """Dump a Sage table's data to file using a given format
+
+       TABLE_NAME is the name of the table to dump.
+
+       SAVE_TO is the name of the file to dump to.
+    """
     logger.info(f'Table name: {table_name}')
     logger.info(f'Output file: {save_to}')
     logger.info(f'Output format: {output_format}')
@@ -60,6 +66,15 @@ def dump_table(manager: Manager, table_name: str, save_to: str, output_format: O
 @click.argument('output-format', type=OutputFormatType(OutputFormatEnum))
 @pass_manager
 def dump_table(manager: Manager, output_directory: str, output_format: OutputFormatEnum):
+    """Dump all Sage tables data to a directory using a given format
+
+       Each table is saved to an individual file in the OUTPUT_DIRECTORY
+
+       Filename is snake-cased, lower-cased table name
+
+       OUTPUT_DIRECTORY is the name of the directory to dump to.
+    """
+
     logger.info(f'Output directory: {output_directory}')
     logger.info(f'Output format: {output_format}')
     try:
@@ -73,6 +88,13 @@ def dump_table(manager: Manager, output_directory: str, output_format: OutputFor
 @click.argument('save-to', type=click.Path(exists=False, dir_okay=False, file_okay=True))
 @pass_manager
 def dump_table_schema(manager: Manager, table_name: str, save_to: str):
+    """Dump a Sage table's schema to a text file
+
+       TABLE_NAME is the name of the table to dump.
+
+       SAVE_TO is the name of the file to dump to.
+    """
+
     logger.info(f'Table name: {table_name}')
     logger.info(f'Output file: {save_to}')
     try:
@@ -85,6 +107,15 @@ def dump_table_schema(manager: Manager, table_name: str, save_to: str):
 @click.argument('output-directory', type=click.Path(exists=True, dir_okay=True, file_okay=False), )
 @pass_manager
 def dump_table_schemas(manager: Manager, output_directory: str):
+    """Dump all Sage table schemas to a directory
+
+       Each table is saved to an individual text file in the OUTPUT_DIRECTORY
+
+       Filename is snake-cased, lower-cased table name .txt extension
+
+       OUTPUT_DIRECTORY is the name of the directory to dump to.
+    """
+
     logger.info(f'Output Directory: {output_directory}')
     try:
         manager.dump_table_schemas(output_directory)
@@ -96,6 +127,11 @@ def dump_table_schemas(manager: Manager, output_directory: str):
 @click.argument('save-to', type=click.Path(exists=False), )
 @pass_manager
 def dump_table_names(manager: Manager, save_to: str):
+    """Dump all Sage table names to a text file
+
+       SAVE_TO is the name of the file to dump to.
+    """
+
     logger.info(f'Output file: {save_to}')
     try:
         manager.dump_table_names(save_to)
@@ -107,6 +143,11 @@ def dump_table_names(manager: Manager, save_to: str):
 @click.argument('save-to', type=click.Path(exists=False), )
 @pass_manager
 def dump_table_counts(manager: Manager, save_to: str):
+    """Dump all Sage table counts to a text file
+
+       SAVE_TO is the name of the file to dump to.
+    """
+
     logger.info(f'Output file: {save_to}')
     try:
         manager.dump_table_counts(save_to)
@@ -118,6 +159,11 @@ def dump_table_counts(manager: Manager, save_to: str):
 @click.argument('table_name', type=str)
 @pass_manager
 def dump_table_count(manager: Manager, table_name: str):
+    """Dump a Sage table's count to STDOUT
+
+       TABLE_NAME is the name of the table to count.
+    """
+
     logger.info(f'Table Name: {table_name}')
     try:
         _count = manager.dump_table_count(table_name)
@@ -131,6 +177,13 @@ def dump_table_count(manager: Manager, table_name: str):
 @click.argument('save-to', type=click.Path(exists=False, dir_okay=False, file_okay=True))
 @pass_manager
 def dump_table_rest_schema(manager: Manager, table_name: str, save_to: str):
+    """Create a flask-rest-jsonapi schema file
+
+       TABLE_NAME is the name of the table.
+
+       SAVE_TO is the name of the file to dump to.
+    """
+
     logger.info(f'Table Name: {table_name}')
     logger.info(f'Output file: {save_to}')
     try:
@@ -143,6 +196,15 @@ def dump_table_rest_schema(manager: Manager, table_name: str, save_to: str):
 @click.argument('output-directory', type=click.Path(exists=True, dir_okay=True, file_okay=False), )
 @pass_manager
 def dump_table_rest_schemas(manager: Manager, output_directory: str):
+    """Create a flask-rest-jsonapi schema file for all Sage tables
+
+       Each table is saved to an individual python file in the OUTPUT_DIRECTORY
+
+       Filename is snake-cased, lower-cased table name .py extension
+
+       OUTPUT_DIRECTORY is the name of the directory to dump to.
+    """
+
     logger.info(f'Output Directory: {output_directory}')
     try:
         manager.dump_table_rest_schemas(output_directory)
